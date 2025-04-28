47d96ba7d09eba158cfc7894e4a791ba1ae0b223
Kadiatoubalde <baldekadi2001@gmail.com>
2025-04-28 17:24:15 +0000

    ProjetReservation

--- src/main/java/org/reservation_backend/controller/PaiementController.java
+++ src/main/java/org/reservation_backend/controller/PaiementController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/paiement")
+@RequestMapping("api/")
 public class PaiementController {
 	
 	@Autowired
